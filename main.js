334b6bdde4b6f496e52be0241e832d1ecbf18c62
Nishant Gaharwar <iamnishantgaharwar@gmail.com>
2023-03-17 05:47:50 +0530

    Solve undefine error.

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ const getRandomColor = (()=>{
     const letter = "123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-        color += letter[Math.floor(Math.random() * 16)]; // Math floor function is to round the value to integer and Math random will generate random value.
+        color = color + letter[Math.floor(Math.random() * 15)]; // Math floor function is to round the value to integer and Math random will generate random value.
     }
     return color;
 })
+
 
 const changeColor = document.getElementById("changeColor");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
     changeColor.style.backgroundColor = getRandomColor(); // take random color
+    console.log(getRandomColor());
 });
+
+
 
 
 
